510c6ffa529d8655e4d85f18b34b022e8d6148e7
Alex Chumbley <chumbleya@gmail.com>
2013-12-21 04:22:28 +0000

    Some Skele API code

---
+++ app/api/colleges.py
@@ -0,0 +1,62 @@
+#------------------------------------------------
+#Imports go here
+from flask import jsonify, abort, request, make_response, url_for, g, flash, redirect, render_template
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal, marshal_with
+from app import app
+
+
+#------------------------------------------------
+
+api = Api(app)
+
+colege_fields = {
+
+}
+
+def collegeExists(id):
+  pass
+
+def getCollege(id):
+  pass
+
+def addCollege(user):
+  pass
+
+def updateCollege(**kwargs):
+  pass
+
+def deleteCollege(id):
+  pass
+
+def getAllColleges():
+  pass
+
+class CollegeAPI(Resource):
+  def __init__(self):
+    pass
+
+  def get(self, id):
+    pass
+
+  def put(self, id):
+    pass
+
+  def delete(self, id):
+    pass
+
+class CollegeListAPI(Resource):
+  def __init__(self):
+    pass
+
+  def get(self):
+    pass
+
+  def post(self):
+    pass
+
+api.add_resource(CollegeAPI, '/api/college/<int:id>', endpoint = 'college')
+#api.add_resource(CollegeAPI, '/api/college/<string:name>', endpoint = 'college')
+api.add_resource(CollegeListAPI, '/api/colleges/', endpoint = 'colleges')
+
+
+
---
+++ app/api/users.py
@@ -0,0 +1,61 @@
+#------------------------------------------------
+#Imports go here
+from flask import jsonify, abort, request, make_response, url_for, g, flash, redirect, render_template
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal, marshal_with
+from app import app
+
+
+#------------------------------------------------
+
+api = Api(app)
+
+user_fields = {
+
+}
+
+def userExists(id):
+  pass
+
+def getUser(id):
+  pass
+
+def addUser(user):
+  pass
+
+def updateUser(**kwargs):
+  pass
+
+def deleteUser(id):
+  pass
+
+def getAllUsersByCollege(college):
+  pass
+
+class UserAPI(Resource):
+  def __init__(self):
+    pass
+
+  def get(self, id):
+    pass
+
+  def put(self, id):
+    pass
+
+  def delete(self, id):
+    pass
+
+class UserListAPI(Resource):
+  def __init__(self):
+    pass
+
+  def get(self):
+    pass
+
+  def post(self):
+    pass
+
+api.add_resource(UserAPI, '/api/user/<int:id>', endpoint = 'user')
+api.add_resource(UserListAPI, '/api/users/', endpoint = 'users')
+
+
+
