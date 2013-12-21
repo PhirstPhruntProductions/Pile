3dbaa4bd7a7d117b5efaf61bba03140b6cf45945
Alex Chumbley <chumbleya@gmail.com>
2013-12-21 04:20:17 +0000

    Skele code for API

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#-----URL Routes for website
 from app.views import *
-from app.models import *
-from app.api import *
+
+#-----Models form DB
+from models.user import *
+from models.college import *
+from models.item import *
+
+#-----API Support
+from api.users import *
+from api.colleges import *
+from api.items import *
