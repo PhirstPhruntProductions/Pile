4d58c5fa59fd0640bffa1b7569b6301a5c18d03c
Alex Chumbley <chumbleya@gmail.com>
2014-01-19 22:14:59 +0000

    Abstracted config code

---
+++ config.py
@@ -0,0 +1,23 @@
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#-----Config the app
+SECRET_KEY = 'ppp_dev'
+
+#-----Config Mail
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = "465"
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'yourfriendlykappas@gmail.com'
+MAIL_PASSWORD = 'CampusStew18'
+
+#-----Config Database
+SQLALCHEMY_DATABASE_URI = 'mysql://achumbley:CampusStew18@localhost/dev'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+#-----Config Admins
+ADMINS = ['chumbleya@gmail.com', 'hreinmit@gmail.com']
+
+#-----Config Upload folder
+UPLOAD_FOLDER = os.path.realpath('./app') + '/uploads'
