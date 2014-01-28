98e702c0416a353368315fe24d6e5d560d2afec5
Alex Chumbley <chumbleya@gmail.com>
2014-01-28 19:28:51 +0000

    Fixed indentation

--- app/models/item.py
+++ app/models/item.py
@@ -2,13 +2,13 @@ from app import db
 from datetime import datetime
 
 class Item(db.Model):
-	__tablename__ = 'items'
-	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(40))
-	description = db.Column(db.String(300))
-	sellerID = db.Column(db.Integer, db.ForeignKey('student.id'))
-	itemtype = db.Column(db.String(50))
-	picture = db.Column(Photo(root="/Pile/app/Images",formats = {'big':'500 x 500'}))
-	dateposted = db.Column(db.DateTime, default = db.func.now())
+  __tablename__ = 'items'
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(40))
+  description = db.Column(db.String(300))
+  sellerID = db.Column(db.Integer, db.ForeignKey('student.id'))
+  itemtype = db.Column(db.String(50))
+  picture = db.Column(Photo(root="/Pile/app/Images",formats = {'big':'500 x 500'}))
+  dateposted = db.Column(db.DateTime, default = db.func.now())
 
 
