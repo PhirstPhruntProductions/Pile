8acc032b075881cb1c78fb31f6741f4fa49830c8
Alex Chumbley <achumbley@ip-10-9-183-147.ec2.internal>
2014-02-19 15:53:47 +0000

    Changed requirements

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
-
-#import logging
-#from logging import FileHandler
-#file_handler = FileHandler('mylog.log')
-#file_handler.setLevel(logging.DEBUG)
-#app.logger.addHandler(file_handler)
 from pile import app
 
 
 if __name__ == "__main__":
-  #try:
     app.run()
-  #except Exception:
-  #  app.logger.exception('Failed')
